27ff527cd8339d525435a0bf6e2dc6d3f32a6f18
pursue_ct <truestyle2005@163.com>
2013-11-11 09:04:54 +0800

    not special commit. Add some code after...

---
+++ vbo/controller/post/MHCPostModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  MHCPostModalViewController.h
+//  vbo
+//
+//  Created by Pursue_finky on 13-11-6.
+//  Copyright (c) 2013年 BmwDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHCPostModalViewController : UIViewController
+
+@end
---
+++ vbo/controller/post/MHCPostModalViewController.m
@@ -0,0 +1,38 @@
+//
+//  MHCPostModalViewController.m
+//  vbo
+//
+//  Created by Pursue_finky on 13-11-6.
+//  Copyright (c) 2013年 BmwDev. All rights reserved.
+//
+
+#import "MHCPostModalViewController.h"
+
+@interface MHCPostModalViewController ()
+
+@end
+
+@implementation MHCPostModalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
