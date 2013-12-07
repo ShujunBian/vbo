aa7fa0b5bd478a3c2d4a8876830fca18dfd47455
Emerosnwade <emerson931216@gmail.com>
2013-12-07 14:25:04 +0800

    增加简单评论和转发页面 答辩使用

---
+++ vbo/controller/cast/TCommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCommentViewController.h
+//  vbo
+//
+//  Created by Emerson on 13-12-7.
+//  Copyright (c) 2013年 BmwDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCommentViewController : UIViewController
+
+@end
---
+++ vbo/controller/cast/TRepostViewController.h
@@ -0,0 +1,13 @@
+//
+//  TRepostViewController.h
+//  vbo
+//
+//  Created by Emerson on 13-12-7.
+//  Copyright (c) 2013年 BmwDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRepostViewController : UIViewController
+
+@end
